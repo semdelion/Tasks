87f88e57129908ff376bea01f94e6fa5bb828c16
Semdelion <semdelion@gmail.com>
2024-01-19 16:45:18 +0700

    29

--- Kotlin/CodeSignal/src/TheCore/_4_LoopTunnel.kt
+++ Kotlin/CodeSignal/src/TheCore/_4_LoopTunnel.kt
@@ -1,12 +1,14 @@
 package TheCore
 
+import kotlin.math.pow
+
 object _4_LoopTunnel {
     fun run() {
         println("25 Least Factorial: ${_4_LoopTunnel.solution25(29)}")
         println("26 Count Sum of Two Representations 2: ${_4_LoopTunnel.solution26(n = 6, l = 2, r = 4)}")
         println("27 Magical Well: ${_4_LoopTunnel.solution27(a = 1, b = 2, n = 2)}")
         println("28 Lineup: ${_4_LoopTunnel.solution28("LLARL")}")
-
+        println("29 Lineup: ${_4_LoopTunnel.solution29(456, 1734)}")
 
     }
 
@@ -29,7 +31,7 @@ object _4_LoopTunnel {
     fun solution25(n: Int): Int {
         var result = 1
         var factorial = 1
-        while(result < n) {
+        while (result < n) {
             result *= ++factorial
         }
         return result
@@ -48,11 +50,11 @@ object _4_LoopTunnel {
 
     There are just two ways to write 6 as A + B, where 2 ≤ A ≤ B ≤ 4: 6 = 2 + 4 and 6 = 3 + 3.*/
     fun solution26(n: Int, l: Int, r: Int): Int {
-        if(2 * l > n || 2 * r < n)
+        if (2 * l > n || 2 * r < n)
             return 0
-        if(r + l == n)
+        if (r + l == n)
             return (r - l) / 2 + 1
-        return if(r + l < n)
+        return if (r + l < n)
             (2 * r - n) / 2 + 1
         else
             (n - 2 * l) / 2 + 1
@@ -72,8 +74,8 @@ object _4_LoopTunnel {
     You will cast your first marble and get $2, after which the numbers will become 2 and 3. When you cast your second marble, the well will give you $6. Overall, you'll make $8. So, the output is 8.*/
     fun solution27(a: Int, b: Int, n: Int): Int {
         var result = 0
-        for(i in 0 until n) {
-            result += (i+a)*(i+b)
+        for (i in 0 until n) {
+            result += (i + a) * (i + b)
         }
         return result
     }
@@ -97,13 +99,48 @@ object _4_LoopTunnel {
     fun solution28(commands: String): Int {
         var state = true
         var count = 0
-        for(command in commands) {
-            if(command != 'A')
+        for (command in commands) {
+            if (command != 'A')
                 state = !state
-            if(state)
+            if (state)
                 count++
         }
         return count
+    }
+
+    /**
+     * Addition Without Carrying
+     * @see "https://app.codesignal.com/arcade/code-arcade/loop-tunnel/xzeZqCQjpfDJuN72S"
+     * @return Addition Without Carrying
+     * * @sample A little child is studying arithmetic. They have just learned how to add two integers, written one below another, column by column. But the child always forgets about the important part - carrying.
+
+    Given two integers, your task is to find the result that the child will get.
+
+    Note: The child had learned from this site, so feel free to check it out too if you are not familiar with column addition.
+
+    Example
+
+    For param1 = 456 and param2 = 1734, the output should be
+    solution(param1, param2) = 1180.
+
+    456
+    1734
+    + ____
+    1180
+    The child performs the following operations from right to left:
+
+    6 + 4 = 10 but the child forgets about carrying the 1 and just writes down the 0 in the last column
+    5 + 3 = 8
+    4 + 7 = 11 but the child forgets about the leading 1 and just writes down 1 under 4 and 7.
+    There is no digit in the first number corresponding to the leading digit of the second one, so the child imagines that 0 is written before 456. Thus, they get 0 + 1 = 1.*/
+    fun solution29(param1: Int, param2: Int): Int {
+        var result = 0
+        val count = Math.max(param1, param2).toString().length
+        for (i in 0 until count) {
+            val div = Math.pow(10.0, i.toDouble()).toInt()
+            result += (((param1 / div) % 10 + (param2 / div) % 10) % 10) * div
+        }
+        return result
     }
 
 
