65936fce4dcde5d0890a7518c87af74fdc23cd1a
Semdelion <semdelion@gmail.com>
2024-01-27 10:35:19 +0700

    47, 48

--- Kotlin/CodeSignal/src/TheCore/_6_LabyrinthOfNestedLoops.kt
+++ Kotlin/CodeSignal/src/TheCore/_6_LabyrinthOfNestedLoops.kt
@@ -6,6 +6,9 @@ object _6_LabyrinthOfNestedLoops {
         println("44 Is Sum of Consecutive 2: ${_6_LabyrinthOfNestedLoops.solution44(9)}")
         println("45 Square Digits Sequence: ${_6_LabyrinthOfNestedLoops.solution45(16)}")
         println("46 Pages Numbering With Ink: ${_6_LabyrinthOfNestedLoops.solution46(21, 5)}")
+        println("47 Comfortable Numbers: ${_6_LabyrinthOfNestedLoops.solution47(10, 12)}")
+        println("48 Weak Numbers: ${_6_LabyrinthOfNestedLoops.solution48(9)}")
+        // println("49 Weak Numbers: ${_6_LabyrinthOfNestedLoops.solution47(10,12)}")
     }
 
     /**
@@ -122,4 +125,133 @@ object _6_LabyrinthOfNestedLoops {
         }
         return --page
     }
+
+    /**
+     * Comfortable Numbers
+     * @see "https://app.codesignal.com/arcade/code-arcade/labyrinth-of-nested-loops/pNfGgNk46YZ4c4RW5"
+     * @return Comfortable Numbers
+     * * @sample Let's say that number a feels comfortable with number b if a ≠ b and b lies in the segment [a - s(a), a + s(a)], where s(x) is the sum of x's digits.
+
+    How many pairs (a, b) are there, such that a < b, both a and b lie on the segment [l, r], and each number feels comfortable with the other (so a feels comfortable with b and b feels comfortable with a)?
+
+    Example
+
+    For l = 10 and r = 12, the output should be
+    solution(l, r) = 2.
+
+    Here are all values of s(x) to consider:
+
+    s(10) = 1, so 10 is comfortable with 9 and 11;
+    s(11) = 2, so 11 is comfortable with 9, 10, 12 and 13;
+    s(12) = 3, so 12 is comfortable with 9, 10, 11, 13, 14 and 15.
+    Thus, there are 2 pairs of numbers comfortable with each other within the segment [10; 12]: (10, 11) and (11, 12).*/
+    fun solution47(l: Int, r: Int): Int {
+        var count = 0
+        for (a in l until r) {
+            for (b in a + 1..r) {
+                if (isComfort(a, b) && isComfort(b, a))
+                    count++
+            }
+        }
+        return count
+    }
+
+    private fun isComfort(a: Int, b: Int): Boolean {
+        val digits = a.toString().map { x -> x.toString().toInt() }.sum()
+        return a != b && (a - digits <= b) && (b <= a + digits)
+    }
+
+    /**
+     * Weak Numbers
+     * @see "https://app.codesignal.com/arcade/code-arcade/labyrinth-of-nested-loops/oL7YuygJKtMSNLeJn"
+     * @return Weak Numbers
+     * * @sample We define the weakness of number x as the number of positive integers smaller than x that have more divisors than x.
+
+    It follows that the weaker the number, the greater overall weakness it has. For the given integer n, you need to answer two questions:
+
+    what is the weakness of the weakest numbers in the range [1, n]?
+    how many numbers in the range [1, n] have this weakness?
+    Return the answer as an array of two elements, where the first element is the answer to the first question, and the second element is the answer to the second question.
+
+    Example
+
+    For n = 9, the output should be
+    solution(n) = [2, 2].
+
+    Here are the number of divisors and the specific weakness of each number in range [1, 9]:
+
+    1: d(1) = 1, weakness(1) = 0;
+    2: d(2) = 2, weakness(2) = 0;
+    3: d(3) = 2, weakness(3) = 0;
+    4: d(4) = 3, weakness(4) = 0;
+    5: d(5) = 2, weakness(5) = 1;
+    6: d(6) = 4, weakness(6) = 0;
+    7: d(7) = 2, weakness(7) = 2;
+    8: d(8) = 4, weakness(8) = 0;
+    9: d(9) = 3, weakness(9) = 2.
+    As you can see, the maximal weakness is 2, and there are 2 numbers with that weakness level.*/
+    fun solution48(n: Int): MutableList<Int> {
+        val listOfDiv = mutableListOf<Int>()
+        val weakness = mutableListOf<Int>()
+        for (i in 1..n) {
+            val div = countDivisors(i)
+            weakness.add(listOfDiv.count { x -> x > div })
+            listOfDiv.add(div)
+        }
+        return mutableListOf(weakness.maxOf { it }, weakness.count { x -> x == weakness.maxOf { it } })
+    }
+
+    private fun countDivisors(n: Int): Int {
+        var count = 0
+        var i = 1
+        while (i * i <= n) {
+            if (n % i == 0) {
+                count += if (i * i < n) 2 else 1
+            }
+            i++
+        }
+        return count
+    }
+
+    /**
+     * Rectangle Rotation
+     * @see "https://app.codesignal.com/arcade/code-arcade/labyrinth-of-nested-loops/tuKA5f3zpzkKKejJx"
+     * @return Rectangle Rotation
+     * * @sample A rectangle with sides equal to even integers a and b is drawn on the Cartesian plane. Its center (the intersection point of its diagonals) coincides with the point (0, 0), but the sides of the rectangle are not parallel to the axes; instead, they are forming 45 degree angles with the axes.
+
+    How many points with integer coordinates are located inside the given rectangle (including on its sides)?
+
+    Example
+
+    For a = 6 and b = 4, the output should be
+    solution(a, b) = 23.
+
+    The following picture illustrates the example, and the 23 points are marked green.*/
+    /*fun solution49(a: Int, b: Int): Int {
+
+    }*/
+
+    /**
+     * Crossword Formation
+     * @see "https://app.codesignal.com/arcade/code-arcade/labyrinth-of-nested-loops/W5Sq7taLSzNHh8GiF"
+     * @return Crossword Formation
+     * * @sample You're a crossword fanatic, and have finally decided to try and create your own. However, you also love symmetry and good design, so you come up with a set of rules they should follow:
+
+    the crossword must contain exactly four words;
+    these four words should form four pairwise intersections;
+    all words must be written either left-to-right or top-to-bottom;
+    the area of the rectangle formed by empty cells inside the intersections isn't equal to zero.
+    Given 4 words, find the number of ways to make a crossword following the above-described rules. Note that two crosswords which differ by rotation are considered different.
+
+    Example
+
+    For words = ["crossword", "square", "formation", "something"], the output should be
+    solution(words) = 6.
+
+    The six crosswords can be formed as shown below:*/
+    /*  fun solution50(words: MutableList<String>): Int {
+
+      }*/
+
+
 }
