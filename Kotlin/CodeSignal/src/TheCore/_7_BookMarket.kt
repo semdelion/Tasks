b5f4390ba8611a9c518107954b29dbf9ac209f1b
Semdelion <semdelion@gmail.com>
2024-01-30 18:15:24 +0700

    52

--- Kotlin/CodeSignal/src/TheCore/_7_BookMarket.kt
+++ Kotlin/CodeSignal/src/TheCore/_7_BookMarket.kt
@@ -3,6 +3,7 @@ package TheCore
 object _7_BookMarket {
     fun run() {
         println("51 Enclose In Brackets: ${_7_BookMarket.solution51("abacaba")}")
+        println("52 Proper Noun Correction: ${_7_BookMarket.solution52("pARiS")}")
 
 
     }
@@ -18,4 +19,24 @@ object _7_BookMarket {
     For inputString = "abacaba", the output should be
     solution(inputString) = "(abacaba)".*/
     fun solution51(inputString: String): String  = "($inputString)"
+
+
+    /**
+     * Proper Noun Correction
+     * @see "https://app.codesignal.com/arcade/code-arcade/book-market/2nSEQ8CGoddTJtnbo"
+     * @return Proper Noun Correction
+     * * @sample Proper nouns always begin with a capital letter, followed by small letters.
+
+    Correct a given proper noun so that it fits this statement.
+
+    Example
+
+    For noun = "pARiS", the output should be
+    solution(noun) = "Paris";
+    For noun = "John", the output should be
+    solution(noun) = "John".*/
+    fun solution52(noun: String): String = noun.first().uppercaseChar() + noun.substring(1).lowercase()
+
+
+
 }
