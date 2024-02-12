fa181900db966e6a5018d3f9857ef856c3114bef
Semdelion <semdelion@gmail.com>
2024-02-12 19:44:12 +0700

    66

--- Kotlin/CodeSignal/src/theCore/_8_MirrorLake.kt
+++ Kotlin/CodeSignal/src/theCore/_8_MirrorLake.kt
@@ -281,4 +281,31 @@ object _8_MirrorLake {
         return maxKeys.maxByOrNull { it.key }?.key ?:0
     }
 
+
+
+    /**
+     * Number of Clans
+     * @see "https://app.codesignal.com/arcade/code-arcade/mirror-lake/BLbGNY3kEcvKjBCFC"
+     * @return Number of Clans
+     * * @sample Let's call two integers A and B friends if each integer from the array divisors is either a divisor of both A and B or neither A nor B. If two integers are friends, they are said to be in the same clan. How many clans are the integers from 1 to k, inclusive, broken into?
+
+    Example
+
+    For divisors = [2, 3] and k = 6, the output should be
+    solution(divisors, k) = 4.
+
+    The numbers 1 and 5 are friends and form a clan, 2 and 4 are friends and form a clan, and 3 and 6 do not have friends and each is a clan by itself. So the numbers 1 through 6 are broken into 4 clans.
+     */
+    fun solution66(divisors: MutableList<Int>, k: Int): Int {
+        val clans = mutableSetOf<Int>()
+        for (i in 1..k) {
+            var clan = 0
+            for (divisor in divisors) {
+                clan = (clan * 10) + if (i % divisor == 0) 1 else 0
+            }
+            clans.add(clan)
+        }
+        return clans.size
+    }
+
 }
