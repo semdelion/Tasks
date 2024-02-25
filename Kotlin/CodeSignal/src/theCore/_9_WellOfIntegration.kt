13131f6027648f468cedc9666761e6a0dad8c9da
Semdelion <semdelion@gmail.com>
2024-02-25 10:56:43 +0700

    77

--- Kotlin/CodeSignal/src/theCore/_9_WellOfIntegration.kt
+++ Kotlin/CodeSignal/src/theCore/_9_WellOfIntegration.kt
@@ -31,6 +31,7 @@ object _9_WellOfIntegration {
         )
         println("75 Elections Winners: ${_9_WellOfIntegration.solution75(mutableListOf(2, 3, 5, 2), 3)}")
         println("76 Add Two Digits: ${solution76(1234, 2)}")
+        println("77 Are Similar?: ${solution77(mutableListOf(1, 2, 3), mutableListOf(2, 1, 3))}")
         // println("79 Alphabet Subsequence: ${_9_WellOfIntegration.solution79(mutableListOf(0, -1, 0, -1, 0, -1))}")
     }
 
@@ -302,5 +303,50 @@ object _9_WellOfIntegration {
         if (stNumber.length > width)
             return stNumber.substring(stNumber.length - width);
         return stNumber
+    }
+
+    /**
+     * Are Similar?
+     * @see "https://app.codesignal.com/arcade/code-arcade/well-of-integration/xYXfzQmnhBvEKJwXP"
+     * @return Are Similar?
+     * * @sample Two arrays are called similar if one can be obtained from another by swapping at most one pair of elements in one of the arrays.
+
+    Given two arrays a and b, check whether they are similar.
+
+    Example
+
+    For a = [1, 2, 3] and b = [1, 2, 3], the output should be
+    solution(a, b) = true.
+
+    The arrays are equal, no need to swap any elements.
+
+    For a = [1, 2, 3] and b = [2, 1, 3], the output should be
+    solution(a, b) = true.
+
+    We can obtain b from a by swapping 2 and 1 in b.
+
+    For a = [1, 2, 2] and b = [2, 1, 1], the output should be
+    solution(a, b) = false.
+
+    Any swap of any two elements either in a or in b won't make a and b equal.*/
+    fun solution77(a: MutableList<Int>, b: MutableList<Int>): Boolean {
+        var difIndex = -1
+        for (index in a.indices) {
+            if (a[index] != b[index]) {
+                if (difIndex == -2)
+                    return false
+
+                difIndex = if (difIndex == -1) {
+                    index
+                } else if (a[difIndex] == b[index] && b[difIndex] == a[index]) {
+                    -2
+                } else
+                    return false
+            }
+        }
+        if (difIndex >= 0)
+            return false
+
+        return true
     }
 }
