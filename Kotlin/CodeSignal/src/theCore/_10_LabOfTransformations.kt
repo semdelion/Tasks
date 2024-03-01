e031adf14e055cfa41bfaa2b20980cc4b00803c6
Semdelion <semdelion@gmail.com>
2024-03-01 19:12:07 +0700

    81

--- Kotlin/CodeSignal/src/theCore/_10_LabOfTransformations.kt
+++ Kotlin/CodeSignal/src/theCore/_10_LabOfTransformations.kt
@@ -3,7 +3,7 @@ package theCore
 object _10_LabOfTransformations {
     fun run() {
         println("80 Character Parity: ${_10_LabOfTransformations.solution80('5')}")
-
+        println("81 Reflect String: ${_10_LabOfTransformations.solution81("abcdefghijklmnopqrstuvwxyz")}")
     }
 
     /**
@@ -25,5 +25,23 @@ object _10_LabOfTransformations {
             return "not a digit"
 
         return if(symbol.code%2==0) "even" else "odd"
+    }
+
+    /**
+     * Reflect String
+     * @see "https://app.codesignal.com/arcade/code-arcade/lab-of-transformations/8nAgfjhDvKCpxwGWF"
+     * @return Reflect String
+     * * @sample Define an alphabet reflection as follows: a turns into z, b turns into y, c turns into x, ..., n turns into m, m turns into n, ..., z turns into a.
+
+    Define a string reflection as the result of applying the alphabet reflection to each of its characters.
+
+    Reflect the given string.
+
+    Example
+
+    For inputString = "name", the output should be
+    solution(inputString) = "mznv".*/
+    fun solution81(inputString: String): String {
+        return inputString.map { ('z'.code - (it.code - 'a'.code)).toChar() }.joinToString("")
     }
 }
