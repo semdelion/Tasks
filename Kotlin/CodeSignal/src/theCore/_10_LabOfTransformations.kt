e5790e1b6f2cde82ed708cd00ee3022f0b31ee21
Semdelion <semdelion@gmail.com>
2024-03-03 11:56:36 +0700

    83

--- Kotlin/CodeSignal/src/theCore/_10_LabOfTransformations.kt
+++ Kotlin/CodeSignal/src/theCore/_10_LabOfTransformations.kt
@@ -5,6 +5,7 @@ object _10_LabOfTransformations {
         println("80 Character Parity: ${_10_LabOfTransformations.solution80('5')}")
         println("81 Reflect String: ${_10_LabOfTransformations.solution81("abcdefghijklmnopqrstuvwxyz")}")
         println("82 New Numeral System: ${_10_LabOfTransformations.solution82('G')}")
+        println("83 Cipher 26: ${_10_LabOfTransformations.solution83("taiaiaertkixquxjnfxxdh")}")
     }
 
     /**
@@ -22,10 +23,10 @@ object _10_LabOfTransformations {
     For symbol = 'q', the output should be
     solution(symbol) = "not a digit".*/
     fun solution80(symbol: Char): String {
-        if(!symbol.isDigit())
+        if (!symbol.isDigit())
             return "not a digit"
 
-        return if(symbol.code%2==0) "even" else "odd"
+        return if (symbol.code % 2 == 0) "even" else "odd"
     }
 
     /**
@@ -71,4 +72,33 @@ object _10_LabOfTransformations {
         return result
     }
 
+    /**
+     * Cipher 26
+     * @see "https://app.codesignal.com/arcade/code-arcade/lab-of-transformations/JeDDnToFtitiiWDZn"
+     * @return Cipher 26
+     * * @sample You've intercepted an encrypted message, and you are really curious about its contents. You were able to find out that the message initially contained only lowercase English letters, and was encrypted with the following cipher:
+
+    Let all letters from 'a' to 'z' correspond to the numbers from 0 to 25, respectively.
+    The number corresponding to the ith letter of the encrypted message is then equal to the sum of numbers corresponding to the first i letters of the initial unencrypted message modulo 26.
+    Now that you know how the message was encrypted, implement the algorithm to decipher it.
+
+    Example
+
+    For message = "taiaiaertkixquxjnfxxdh", the output should be
+    solution(message) = "thisisencryptedmessage".
+
+    The initial message "thisisencryptedmessage" was encrypted as follows:
+
+    letter 0: t -> 19 -> t;
+    letter 1: th -> (19 + 7) % 26 -> 0 -> a;
+    letter 2: thi -> (19 + 7 + 8) % 26 -> 8 -> i;
+    etc.*/
+    fun solution83(message: String): String {
+        var sum = 0
+        return  message.map {
+            val dif = (it.code - sum % 26 - 71) % 26
+            sum += dif
+            (dif + 97).toChar()
+        }.joinToString("")
+    }
 }
