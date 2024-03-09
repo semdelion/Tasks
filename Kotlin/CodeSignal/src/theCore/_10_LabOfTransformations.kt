68a3ceb25a213e8007de57dacc28b8df77a7b455
Semdelion <semdelion@gmail.com>
2024-03-10 02:19:22 +0700

    87

--- Kotlin/CodeSignal/src/theCore/_10_LabOfTransformations.kt
+++ Kotlin/CodeSignal/src/theCore/_10_LabOfTransformations.kt
@@ -9,6 +9,7 @@ object _10_LabOfTransformations {
         println("84 Stolen Lunch: ${_10_LabOfTransformations.solution84("you'll n4v4r 6u4ss 8t: cdja")}")
         println("85 Higher Version: ${_10_LabOfTransformations.solution85("1.2.2", "1.2.0")}")
         println("86 Decipher: ${_10_LabOfTransformations.solution86("10197115121")}")
+        println("87 Alphanumeric Less: ${_10_LabOfTransformations.solution87("x11y012", "x011y13")}")
     }
 
     /**
@@ -190,5 +191,105 @@ object _10_LabOfTransformations {
             }
         }
         return result
+    }
+
+    /**
+     * Alphanumeric Less
+     * @see "https://app.codesignal.com/arcade/code-arcade/lab-of-transformations/bBgu7NEfk2GoQuNzf"
+     * @return Alphanumeric Less
+     * * @sample An alphanumeric ordering of strings is defined as follows: each string is considered as a sequence of tokens, where each token is a letter or a number (as opposed to an isolated digit, as is the case in lexicographic ordering). For example, the tokens of the string "ab01c004" are [a, b, 01, c, 004]. In order to compare two strings, we'll first break them down into tokens and then compare the corresponding pairs of tokens with each other (i.e. compare the first token of the first string with the first token of the second string, etc).
+
+    Here is how tokens are compared:
+
+    If a letter is compared with another letter, the usual alphabetical order applies.
+    A number is always considered less than a letter.
+    When two numbers are compared, their values are compared. Leading zeros, if any, are ignored.
+    If at some point one string has no more tokens left while the other one still does, the one with fewer tokens is considered smaller.
+
+    If the two strings s1 and s2 appear to be equal, consider the smallest index i such that tokens(s1)[i] and tokens(s2)[i] (where tokens(s)[i] is the ith token of string s) differ only by the number of leading zeros. If no such i exists, the strings are indeed equal. Otherwise, the string whose ith token has more leading zeros is considered smaller.
+
+    Here are some examples of comparing strings using alphanumeric ordering.
+
+    "a" < "a1" < "ab"
+    "ab42" < "ab000144" < "ab00144" < "ab144" < "ab000144x"
+    "x11y012" < "x011y13"
+    Your task is to return true if s1 is strictly less than s2, and false otherwise.
+
+    Example
+
+    For s1 = "a" and s2 = "a1", the output should be solution(s1, s2) = true;
+
+    These strings have equal first tokens, but since s1 has fewer tokens than s2, it's considered smaller.
+
+    For s1 = "ab" and s2 = "a1", the output should be solution(s1, s2) = false;
+
+    These strings also have equal first tokens, but since numbers are considered less than letters, s1 is larger.
+
+    For s1 = "b" and s2 = "a1", the output should be solution(s1, s2) = false.
+
+    Since b is greater than a, s1 is larger.*/
+    fun solution87(s1: String, s2: String): Boolean {
+        val tokensS1 = getTokens(s1)
+        val tokensS2 = getTokens(s2)
+
+        for (i in 0 until minOf(tokensS1.size, tokensS2.size)) {
+            if (tokensS1[i].first != tokensS2[i].first)
+                return tokensS1[i].first < tokensS2[i].first
+            else {
+                if (tokensS1[i].first == 1) {
+                    if (tokensS1[i].second != tokensS2[i].second)
+                        return tokensS1[i].second[0].code < tokensS2[i].second[0].code
+                } else {
+                    return compareDigits(tokensS1[i].second, tokensS2[i].second) ?: continue
+                }
+            }
+        }
+        if (tokensS1.count() != tokensS2.count())
+            return tokensS1.count() < tokensS2.count()
+
+        for (i in tokensS1.indices)
+            if (tokensS1[i].second.length > tokensS2[i].second.length)
+                return true
+
+        return false
+    }
+
+    private fun getTokens(s: String): List<Pair<Int, String>> {
+        val tokens = mutableListOf<Pair<Int, String>>()
+        var i = 0
+        while (i < s.length) {
+            when {
+                s[i].isLetter() -> {
+                    tokens.add(1 to s[i].toString())
+                }
+                s[i].isDigit() -> {
+                    var token = ""
+                    while (i < s.length && s[i].isDigit()) {
+                        token += s[i]
+                        i++
+                    }
+                    tokens.add(0 to token)
+                    i--
+                }
+            }
+            i++
+        }
+        return tokens
+    }
+
+    private fun compareDigits(s1: String, s2: String): Boolean? {
+        val trim1 = s1.trimStart('0')
+        val trim2 = s2.trimStart('0')
+        if (trim1 != trim2) {
+            if(trim1.length != trim2.length)
+                return trim1.length < trim2.length
+            else {
+                for (j in trim1.indices) {
+                    if (trim1[j] != trim2[j])
+                        return trim1[j] < trim2[j]
+                }
+            }
+        }
+        return null
     }
 }
