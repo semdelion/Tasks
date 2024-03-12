ebd5ae44eb6a594e42fc045254559bb0bc9c975d
Semdelion <semdelion@gmail.com>
2024-03-12 19:07:36 +0700

    89

--- Kotlin/CodeSignal/src/theCore/_11_SpringOfIntegration.kt
+++ Kotlin/CodeSignal/src/theCore/_11_SpringOfIntegration.kt
@@ -3,6 +3,7 @@ package theCore
 object _11_SpringOfIntegration {
     fun run() {
         println("88 Array Conversion: ${_11_SpringOfIntegration.solution88(mutableListOf(1, 2, 3, 4, 5, 6, 7, 8))}")
+        println("89 Array Previous Less: ${_11_SpringOfIntegration.solution89(mutableListOf(3, 5, 2, 4, 5))}")
     }
 
     /**
@@ -34,7 +35,32 @@ object _11_SpringOfIntegration {
         return result[0]
     }
 
+    /**
+     * Array Previous Less
+     * @see "https://app.codesignal.com/arcade/code-arcade/spring-of-integration/MQg7s3dKrP4caN42A"
+     * @return Array Previous Less
+     * * @sample Given array of integers, for each position i, search among the previous positions for the last (from the left) position that contains a smaller value. Store this value at position i in the answer. If no such value can be found, store -1 instead.
 
+    Example
 
+    For items = [3, 5, 2, 4, 5], the output should be
+    solution(items) = [-1, 3, -1, 2, 4].*/
+    fun solution89(items: MutableList<Int>): MutableList<Int> {
+        var flag = false
+        for (i in items.size - 1 downTo 0) {
+            for (j in i - 1 downTo 0) {
+                if(items[j] < items[i]) {
+                    items[i] = items[j]
+                    flag = true
+                    break
+                }
+            }
+            if (!flag)
+                items[i] = -1
 
+            flag = false
+        }
+        items[0] = -1
+        return items
+    }
 }
