aaf595a799dbfd96f464957a3408e2de229df74c
Semdelion <semdelion@gmail.com>
2024-03-28 19:45:01 +0700

    96

--- Kotlin/CodeSignal/src/theCore/_11_SpringOfIntegration.kt
+++ Kotlin/CodeSignal/src/theCore/_11_SpringOfIntegration.kt
@@ -27,6 +27,11 @@ object _11_SpringOfIntegration {
         println("95) Runners Meetings\n" +
                 "      input: s = startPosition = [1, 4, 2], speed = [27, 18, 24]\n" +
                 "      result: ${solution95(mutableListOf(1, 4, 2), mutableListOf(27, 18, 24) )}\n")
+        println("96) Christmas Tree\n" +
+                "      input: s = levelNum = 1, levelHeight = 3\n" +
+                "      result: \n")
+        solution96(2, 3).forEach { println("            $it") }
+        println()
     }
 
     /**
@@ -337,5 +342,107 @@ object _11_SpringOfIntegration {
             }
         }
         return maxOfMeetingCardinality
+    }
+
+    /**
+     * Christmas Tree
+     * @see "https://app.codesignal.com/arcade/code-arcade/spring-of-integration/Hb9Cppx4pCY4d8J5P"
+     * @return Christmas Tree
+     * * @sample It's Christmas time! To share his Christmas spirit with all his friends, the young Christmas Elf decided to send each of them a Christmas e-mail with a nice Christmas tree. Unfortunately, Internet traffic is very expensive in the North Pole, so instead of sending an actual image he got creative and drew the tree using nothing but asterisks ('*' symbols). He has given you the specs (see below) and your task is to write a program that will generate trees following the spec and some initial parameters.
+
+    Here is a formal definition of how the tree should be built, but before you read it the Elf HIGHLY recommends first looking at the examples that follow:
+
+    Each tree has a crown as follows:
+
+     *
+     *
+     ***
+    Define a line as a horizontal group of asterisks and a level as a collection of levelHeight lines stacked one on top of the other.
+
+    Below the crown there are levelNum levels.
+
+    The tree is perfectly symmetrical so all the middle asterisks of the lines lie on the center of the tree.
+
+    Each line of the same level (excluding the first one) has two more asterisks than the previous one (one added to each end);
+
+    The number of asterisks in the first line of each level is chosen as follows:
+
+    the first line of the first level has 5 asterisks;
+    the first line of each consecutive level contains two more asterisks than the first line of the previous level.
+    And finally there is the tree foot which has a height of levelNum and a width of:
+
+    levelHeight asterisks if levelHeight is odd;
+    levelHeight + 1 asterisks if levelHeight is even.
+    Given levelNum and levelHeight, return the Christmas tree of the young elf.
+
+    Example
+
+    For levelNum = 1 and levelHeight = 3, the output should be
+
+    solution(levelNum, levelHeight) =
+    "    *",
+    "    *",
+    "   ***",
+    "  *****",
+    " *******",
+    "*********",
+    "   ***"
+    , which represents the following tree:
+                ___
+         *        |
+         *        |-- the crown
+        ***    ___|
+       *****      |
+      *******     |-- level 1
+     ********* ___|
+        ***    ___|-- the foot
+    For levelNum = 2 and levelHeight = 4, the output should be
+
+    solution(levelNum, levelHeight) =
+    "      *",
+    "      *",
+    "     ***",
+    "    *****",
+    "   *******",
+    "  *********",
+    " ***********",
+    "   *******",
+    "  *********",
+    " ***********",
+    "*************",
+    "    *****",
+    "    *****"
+    , which represents the following tree:
+                    ___
+           *          |
+           *          | -- the crown
+          ***      ___|
+         *****        |
+        *******       | -- level 1
+       *********      |
+      ***********  ___|
+        *******       |
+       *********      | -- level 2
+      ***********     |
+     ************* ___|
+         *****        | -- the foot
+         *****     ___|*/
+    fun solution96(levelNum: Int, levelHeight: Int): MutableList<String> {
+        //the crown
+        val christmasTree = mutableListOf<String>()
+        christmasTree.add(" ".repeat(levelHeight + levelNum) + "*")
+        christmasTree.add(christmasTree[0])
+        christmasTree.add(" ".repeat(levelHeight + levelNum - 1) + "***")
+        //levels
+        for (i in 1..levelNum) {
+            for(j in 1.. levelHeight) {
+                christmasTree.add(" ".repeat(levelHeight + levelNum - i - j) + "*".repeat(1 + i * 2 + j * 2))
+            }
+        }
+        //the foot
+        for (i in 1..levelNum) {
+            christmasTree.add(" ".repeat(levelHeight + levelNum - levelHeight / 2) + "*".repeat(levelHeight + (levelHeight + 1) % 2))
+        }
+        return christmasTree
     }
 }
