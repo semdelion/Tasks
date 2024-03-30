7826a2de1685a948c578c1be226e946c3aef47fe
Semdelion <semdelion@gmail.com>
2024-03-30 18:31:56 +0700

    98

--- Kotlin/CodeSignal/src/theCore/_12_ListBackwoods.kt
+++ Kotlin/CodeSignal/src/theCore/_12_ListBackwoods.kt
@@ -2,20 +2,39 @@ package theCore
 
 object _12_ListBackwoods {
     fun run() {
-       /* println("98) Add Two Digits\n" +
-                "      input:\n" +
-                "      result: ${_12_ListBackwoods.solution98(29)}\n")*/
+        println("98) Extract Matrix Column\n" +
+                "      input: matrix = \n" +
+                "             [1, 1, 1, 2]\n" +
+                "             [0, 5, 0, 4]\n" +
+                "             [2, 1, 3, 6]\n" +
+                "      result: ${solution98(mutableListOf(
+                            mutableListOf(1, 1, 1, 2),
+                            mutableListOf(0, 5, 0, 4),
+                            mutableListOf(2, 1, 3, 6)
+                        ),column = 2)}\n")
+
     }
 
     /**
-     * Add Two Digits
-     * @see "https://app.codesignal.com/arcade/code-arcade/intro-gates/wAGdN6FMPkx7WBq66"
-     * @return Add Two Digits
-     * * @sample You are given a two-digit integer n. Return the sum of its digits.
+     * Extract Matrix Column
+     * @see "https://app.codesignal.com/arcade/code-arcade/list-backwoods/zwXiykHLor6eKHaLY"
+     * @return Extract Matrix Column
+     * * @sample Given a rectangular matrix and an integer column, return an array containing the elements of the columnth column of the given matrix (the leftmost column is the 0th one).
 
     Example
 
-    For n = 29, the output should be
-    solution(n) = 11.*/
-    fun solution98(n: Int): Int = n/10 + n%10
+    For
+
+    matrix = [[1, 1, 1, 2],
+    [0, 5, 0, 4],
+    [2, 1, 3, 6]]
+    and column = 2, the output should be
+    solution(matrix, column) = [1, 0, 3]. */
+    fun solution98(matrix: MutableList<MutableList<Int>>, column: Int): MutableList<Int> {
+        val result = mutableListOf<Int>()
+        for(i in 0 until matrix.count()) {
+            result.add(matrix[i][column])
+        }
+        return result
+    }
 }
