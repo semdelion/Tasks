2b5755fd3bb9bf180b15a409a131a70b3bc50f79
Semdelion <semdelion@gmail.com>
2024-04-05 19:23:36 +0700

    104

--- Kotlin/CodeSignal/src/theCore/_12_ListBackwoods.kt
+++ Kotlin/CodeSignal/src/theCore/_12_ListBackwoods.kt
@@ -87,6 +87,23 @@ object _12_ListBackwoods {
             ), mutableListOf(1, 1, 4, 3)
         ).forEach { println("             $it") }
         println()
+        println("104) Volleyball Positions\n" +
+                "      input: formation = \n" +
+                "             [empty, Alice, empty]\n" +
+                "             [Bob,   empty, Charlie]\n" +
+                "             [empty, Dave,  empty]\n" +
+                "             [Eve,   empty, Frank]\n" +
+                "             k = 3\n" +
+                "      result:")
+        solution104(
+            mutableListOf(
+                mutableListOf("empty", "Alice", "empty"),
+                mutableListOf("Bob", "empty", "Charlie"),
+                mutableListOf("empty", "Dave", "empty"),
+                mutableListOf("Eve", "empty", "Frank")
+            ), k = 3
+        ).forEach { println("             $it") }
+        println()
     }
 
     /**
@@ -301,5 +318,73 @@ object _12_ListBackwoods {
         canvas[y2][x1] = '*'
 
         return canvas
+    }
+
+    /**
+     * Volleyball Positions
+     * @see "https://app.codesignal.com/arcade/code-arcade/list-backwoods/fmYdsYcGfaTu4yTQt"
+     * @return Volleyball Positions
+     * * @sample You are watching a volleyball tournament, but you missed the beginning of the very first game of your favorite team. Now you're curious about how the coach arranged the players on the field at the start of the game.
+
+    The team you favor plays in the following formation:
+
+    0 3 0
+    4 0 2
+    0 6 0
+    5 0 1
+    where positive numbers represent positions occupied by players. After the team gains the serve, its members rotate one position in a clockwise direction, so the player in position 2 moves to position 1, the player in position 3 moves to position 2, and so on, with the player in position 1 moving to position 6.
+
+    Here's how the players change their positions:
+
+
+
+    Given the current formation of the team and the number of times k it gained the serve, find the initial position of each player in it.
+
+    Example
+
+    For
+
+    formation =
+    [["empty",   "Player5", "empty"],
+    ["Player4", "empty",   "Player2"],
+    ["empty",   "Player3", "empty"],
+    ["Player6", "empty",   "Player1"]]
+    and k = 2, the output should be
+
+    solution(formation, k) =
+    [["empty",   "Player1", "empty"],
+    ["Player2", "empty",   "Player3"],
+    ["empty",   "Player4", "empty"],
+    ["Player5", "empty",   "Player6"]]
+    For
+
+    formation =
+    [["empty", "Alice", "empty"],
+    ["Bob",   "empty", "Charlie"],
+    ["empty", "Dave",  "empty"],
+    ["Eve",   "empty", "Frank"]]
+    and k = 6, the output should be
+
+    solution(formation, k) =
+    [["empty", "Alice", "empty"],
+    ["Bob",   "empty", "Charlie"],
+    ["empty", "Dave",  "empty"],
+    ["Eve",   "empty", "Frank"]]*/
+    fun solution104(formation: MutableList<MutableList<String>>, k: Int): MutableList<MutableList<String>> {
+        val k1 = k % 6
+
+        if(k1 == 0)
+            return formation
+
+        val players = mutableListOf(formation[3][2], formation[1][2], formation[0][1], formation[1][0], formation[3][0], formation[2][1])
+
+        formation[3][2] = players[6 - k1]
+        formation[1][2] = players[(7 - k1) % 6]
+        formation[0][1] = players[(8 - k1) % 6]
+        formation[1][0] = players[(9 - k1) % 6]
+        formation[3][0] = players[(10 - k1) % 6]
+        formation[2][1] = players[(11 - k1) % 6]
+
+        return formation
     }
 }
