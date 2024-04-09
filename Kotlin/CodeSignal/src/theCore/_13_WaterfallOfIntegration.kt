f8b6adf897e716c26e59649b400bd79a56fda460
Semdelion <semdelion@gmail.com>
2024-04-09 21:51:57 +0700

    108

--- Kotlin/CodeSignal/src/theCore/_13_WaterfallOfIntegration.kt
+++ Kotlin/CodeSignal/src/theCore/_13_WaterfallOfIntegration.kt
@@ -43,6 +43,23 @@ object _13_WaterfallOfIntegration {
             )
         ).forEach { println("             $it") }
         println()
+        println("108) Box Blur\n" +
+                "      input: image = \n" +
+                "             ${mutableListOf(7, 4, 0, 1)} \n" +
+                "             ${mutableListOf(5, 6, 2, 2)} \n" +
+                "             ${mutableListOf(6, 10, 7, 8)} \n" +
+                "             ${mutableListOf(1, 4, 2, 0)} \n" +
+                "      result:")
+        solution108(
+            mutableListOf(
+                mutableListOf(7, 4, 0, 1),
+                mutableListOf(5, 6, 2, 2),
+                mutableListOf(6, 10, 7, 8),
+
+                mutableListOf(1, 4, 2, 0)
+            )
+        ).forEach { println("             $it") }
+        println()
     }
 
     /**
@@ -121,5 +138,53 @@ object _13_WaterfallOfIntegration {
             }
         }
         return resultMatrix
+    }
+
+    /**
+     * Box Blur
+     * @see "https://app.codesignal.com/arcade/code-arcade/waterfall-of-integration/5xPitc3yT3dqS7XkP"
+     * @return Box Blur
+     * * @sample Last night you partied a little too hard. Now there's a black and white photo of you that's about to go viral! You can't let this ruin your reputation, so you want to apply the box blur algorithm to the photo to hide its content.
+
+    The pixels in the input image are represented as integers. The algorithm distorts the input image in the following way: Every pixel x in the output image has a value equal to the average value of the pixel values from the 3 × 3 square that has its center at x, including x itself. All the pixels on the border of x are then removed.
+
+    Return the blurred image as an integer, with the fractions rounded down.
+
+    Example
+
+    For
+
+    image =
+    [1, 1, 1],
+    [1, 7, 1],
+    [1, 1, 1]
+    the output should be solution(image) = [[1]].
+
+    To get the value of the middle pixel in the input 3 × 3 square: (1 + 1 + 1 + 1 + 7 + 1 + 1 + 1 + 1) = 15 / 9 = 1.66666 = 1. The border pixels are cropped from the final result.
+
+    For
+
+    image =
+    [7, 4,  0, 1],
+    [5, 6,  2, 2],
+    [6, 10, 7, 8],
+    [1, 4,  2, 0]
+    the output should be
+
+    solution(image) = [[5, 4],
+    [4, 4]]
+    There are four 3 × 3 squares in the input image, so there should be four integers in the blurred output. To get the first value: (7 + 4 + 0 + 5 + 6 + 2 + 6 + 10 + 7) = 47 / 9 = 5.2222 = 5. The other three integers are obtained the same way, then the surrounding integers are cropped from the final result.
+     */
+    fun solution108(image: MutableList<MutableList<Int>>): MutableList<MutableList<Int>> {
+        val blurImage = MutableList(image.count() - 2) { MutableList(image[0].count() - 2) { 0 } }
+        for (i in 0..image.count() - 3) {
+            for (j in 0..image[i].count() - 3) {
+                blurImage[i][j] = (
+                        (image[i][j] + image[i][j + 1] + image[i][j + 2] +
+                                image[i + 1][j] + image[i + 1][j + 1] + image[i + 1][j + 2] +
+                                image[i + 2][j] + image[i + 2][j + 1] + image[i + 2][j + 2]) / 9)
+            }
+        }
+        return blurImage
     }
 }
