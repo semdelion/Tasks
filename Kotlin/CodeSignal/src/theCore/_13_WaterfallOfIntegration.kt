3c300bfab536ce6247076f916aee09e8b98f243b
Semdelion <semdelion@gmail.com>
2024-04-10 21:52:59 +0700

    109

--- Kotlin/CodeSignal/src/theCore/_13_WaterfallOfIntegration.kt
+++ Kotlin/CodeSignal/src/theCore/_13_WaterfallOfIntegration.kt
@@ -60,6 +60,24 @@ object _13_WaterfallOfIntegration {
             )
         ).forEach { println("             $it") }
         println()
+        println("109) Contours Shifting\n" +
+                "      input: image = \n" +
+                "             ${mutableListOf(1,   2,  3,  4)} \n" +
+                "             ${mutableListOf(5,   6,  7,  8)} \n" +
+                "             ${mutableListOf(9,  10, 11, 12)} \n" +
+                "             ${mutableListOf(13, 14, 15, 16)} \n" +
+                "             ${mutableListOf(17, 18, 19, 20)} \n" +
+                "      result:")
+        solution109(
+            mutableListOf(
+                mutableListOf(1,   2,  3,   4),
+                mutableListOf(5,   6,  7,   8),
+                mutableListOf(9,   10, 11, 12),
+                mutableListOf(13,  14, 15, 16),
+                mutableListOf(17,  18, 19, 20)
+            )
+        ).forEach { println("             $it") }
+        println()
     }
 
     /**
@@ -186,5 +204,105 @@ object _13_WaterfallOfIntegration {
             }
         }
         return blurImage
+    }
+
+    /**
+     * Contours Shifting
+     * @see "https://app.codesignal.com/arcade/code-arcade/waterfall-of-integration/LfP67YRDw2rxoLmeP"
+     * @return Contours Shifting
+     * * @sample Mark got a rectangular array matrix for his birthday, and now he's thinking about all the fun things he can do with it. He likes shifting a lot, so he decides to shift all of its i-contours in a clockwise direction if i is even, and counterclockwise if i is odd.
+
+    Here is how Mark defines i-contours:
+
+    the 0-contour of a rectangular array as the union of left and right columns as well as top and bottom rows;
+    consider the initial matrix without the 0-contour: its 0-contour is the 1-contour of the initial matrix;
+    define 2-contour, 3-contour, etc. in the same manner by removing 0-contours from the obtained arrays.
+    Implement a function that does exactly what Mark wants to do to his matrix.
+
+    Example
+
+    For
+
+    matrix =
+    [ 1,  2,  3,  4],
+    [ 5,  6,  7,  8],
+    [ 9, 10, 11, 12],
+    [13, 14, 15, 16],
+    [17, 18, 19, 20]
+    the output should be
+
+    solution(matrix) =
+    [ 5,  1,  2,  3],
+    [ 9,  7, 11,  4],
+    [13,  6, 15,  8],
+    [17, 10, 14, 12],
+    [18, 19, 20, 16]
+    For matrix = [[238, 239, 240, 241, 242, 243, 244, 245]],
+    the output should be
+    solution(matrix) = [[245, 238, 239, 240, 241, 242, 243, 244]].
+
+    Note, that if a contour is represented by a 1 × n array, its center is considered to be below it.
+
+    For
+
+    matrix = [[238],
+    [239],
+    [240],
+    [241],
+    [242],
+    [243],
+    [244],
+    [245]]
+    the output should be
+
+    solution(matrix) = [[245],
+    [238],
+    [239],
+    [240],
+    [241],
+    [242],
+    [243],
+    [244]]
+    If a contour is represented by an n × 1 array, its center is considered to be to the left of it.*/
+    fun solution109(matrix: MutableList<MutableList<Int>>): MutableList<MutableList<Int>> {
+        val maxContour = if (matrix.size > 1 && matrix[0].size > 1) Math.round(Math.min(matrix.size, matrix[0].size) /
+                2.0) - 1
+        else 0
+        (0..maxContour.toInt()).forEach {
+            val contourI = icontour(matrix, it)
+            val shiftedContour = shiftContour(contourI, if (it % 2 == 1) 1 else -1)
+            shiftedContour.forEach {
+                matrix[it.first.first][it.first.second] = it.second
+            }
+        }
+        return matrix
+    }
+
+    private fun icontour(inputMatrix: MutableList<MutableList<Int>>, i: Int): MutableList<Pair<Pair<Int, Int>, Int>> {
+        val m = inputMatrix.indices.map { ri ->
+            inputMatrix[ri].indices.map { ci ->
+                Pair(ri, ci) to inputMatrix[ri][ci]
+            }
+        }
+        if ((m.size - 1 - i) == i) {
+            return m[i].subList(i, m[0].size - i).toMutableList()
+        }
+        if ((m[0].size - 1 - i) == i) {
+            return m.subList(i, m.size - i).map { it[i] }.toMutableList()
+        }
+        val topRow = m[i].subList(i, m[0].size - i)
+
+        val rightCol = m.subList(i + 1, m.size - i - 1)
+            .map { it[m[0].size - i - 1]}
+        val bottomRow = (m[0].size - 1 - i).downTo(i).map {m[m.size - 1 - i][it] }
+        val leftCol = (m.size - i - 2).downTo(i+1).map { m[it][i]}
+        val contour = listOf(topRow, rightCol, bottomRow, leftCol).flatten()
+        return contour.toMutableList()
+    }
+
+    private fun shiftContour(contour: MutableList<Pair<Pair<Int, Int>, Int>>, d: Int): MutableList<Pair<Pair<Int, Int>, Int>> {
+        return contour.indices.map {
+            contour[it].first to contour[(it + contour.size + d) % contour.size].second
+        }.toMutableList()
     }
 }
