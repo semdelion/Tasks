f3300314ea928649ee8699071232c4ae2aa27920
Semdelion <semdelion@gmail.com>
2024-04-12 20:29:00 +0700

    111

--- Kotlin/CodeSignal/src/theCore/_13_WaterfallOfIntegration.kt
+++ Kotlin/CodeSignal/src/theCore/_13_WaterfallOfIntegration.kt
@@ -92,6 +92,13 @@ object _13_WaterfallOfIntegration {
                         )
                     )
                 }")
+        println("111) Gravitation\n" +
+                "      input: rows = \n" +
+                "             \"#..##\",\n" +
+                "             \".##.#\",\n" +
+                "             \".#.##\",\n" +
+                "             \".....\",\n" +
+                "      result: ${solution111(mutableListOf("#..##", ".##.#", ".#.##", "....."))}")
     }
 
     /**
@@ -368,5 +375,44 @@ object _13_WaterfallOfIntegration {
             }
         }
         return count
+    }
+
+    /**
+     * Gravitation
+     * @see "https://app.codesignal.com/arcade/code-arcade/waterfall-of-integration/hqrYesGKEaKQnv7Sv"
+     * @return Gravitation
+     * * @sample You are given a vertical box divided into equal columns. Someone dropped several stones from its top through the columns. Stones are falling straight down at a constant speed (equal for all stones) while possible (i.e. while they haven't reached the ground or they are not blocked by another motionless stone). Given the state of the box at some moment in time, find out which columns become motionless first.
+
+    Example
+
+    For
+
+    rows =
+    "#..##",
+    ".##.#",
+    ".#.##",
+    "....."
+    the output should be solution(rows) = [1, 4].
+
+    Check out the image below for better understanding:
+
+     */
+    fun solution111(rows: MutableList<String>): MutableList<Int> {
+        val times = mutableListOf<Int>()
+        for (i in 0 until rows[0].length) {
+            var flag = false
+            var count = 0
+            for (j in 0 until rows.size) {
+                if (rows[j][i] == '#') flag = true
+                if (rows[j][i] == '.' && flag) count++
+            }
+            times.add(count)
+        }
+        val min = times.minOf{it}
+        val result = mutableListOf<Int>()
+        for (i in 0 until times.size) {
+            if (times[i] == min) result.add(i)
+        }
+        return result
     }
 }
