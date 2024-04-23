6b2de740d57a769bfbb7c8c9ce653d157040e620
Semdelion <semdelion@gmail.com>
2024-04-23 21:54:12 +0700

    Add Two Numbers LeetCode

--- Kotlin/CodeSignal/src/algorithms/_2_LeetCode.kt
+++ Kotlin/CodeSignal/src/algorithms/_2_LeetCode.kt
@@ -114,4 +114,66 @@ object _2_LeetCode {
         }
     }
 
+    class ListNode(var `val`: Int) {
+        var next: ListNode? = null
+    }
+
+    /**
+     * Add Two Numbers
+     * @see "https://leetcode.com/problems/add-two-numbers/"
+     * @return Add Two Digits
+     * difficulty = normal
+     * * @sample You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+    You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+
+
+    Example 1:
+
+
+    Input: l1 = [2,4,3], l2 = [5,6,4]
+    Output: [7,0,8]
+    Explanation: 342 + 465 = 807.
+    Example 2:
+
+    Input: l1 = [0], l2 = [0]
+    Output: [0]
+    Example 3:
+
+    Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+    Output: [8,9,9,9,0,0,0,1]
+
+
+    Constraints:
+
+    The number of nodes in each linked list is in the range [1, 100].
+    0 <= Node.val <= 9
+    It is guaranteed that the list represents a number that does not have leading zeros.*/
+    fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
+        var next1 = l1
+        var next2 = l2
+        val result = ListNode(0)
+        var resultNext = result
+        var div = 0
+        while(next1 != null || next2 != null ) {
+            val val1 = next1?.`val` ?: 0
+            val val2 = next2?.`val` ?: 0
+            resultNext.`val` = (div + val1 + val2) % 10
+            div = (div + val1 + val2) / 10
+            next1 = next1?.next
+            next2 = next2?.next
+            if(next1!= null || next2 != null) {
+                resultNext.next = ListNode(0)
+                resultNext = resultNext.next!!
+            }
+        }
+
+        if (div > 0) {
+            resultNext.next = ListNode(div)
+        }
+
+        return result
+    }
+
 }
