5969fe1d160d8f14cc9aa1f86976a12952e2a70a
Semdelion <semdelion@gmail.com>
2024-04-29 12:54:38 +0700

    122

--- Kotlin/CodeSignal/src/theCore/_15_ChessTavern.kt
+++ Kotlin/CodeSignal/src/theCore/_15_ChessTavern.kt
@@ -1,21 +1,28 @@
 package theCore
 
+import kotlin.math.abs
+
 object _15_ChessTavern {
     fun run() {
-       /* println("122) Add Two Digits\n" +
-                "      input:\n" +
-                "      result: ${_15_ChessTavern.solution122(29)}\n")*/
+        println("122) Bishop and Pawn\n" +
+                "      input: bishop = a1, pawn = c3\n" +
+                "      result: ${_15_ChessTavern.solution122("a1", "c3")}\n")
     }
 
     /**
-     * Add Two Digits
-     * @see "https://app.codesignal.com/arcade/code-arcade/intro-gates/wAGdN6FMPkx7WBq66"
-     * @return Add Two Digits
-     * * @sample You are given a two-digit integer n. Return the sum of its digits.
+     * Bishop and Pawn
+     * @see "https://app.codesignal.com/arcade/code-arcade/chess-tavern/6M57rMTFB9MeDeSWo"
+     * @return Bishop and Pawn
+     * * @sample Given the positions of a white bishop and a black pawn on the standard chess board, determine whether the bishop can capture the pawn in one move.
+
+    The bishop has no restrictions in distance for each move, but is limited to diagonal movement. Check out the example below to see how it can move:
 
     Example
 
-    For n = 29, the output should be
-    solution(n) = 11.*/
-    fun solution122(n: Int): Int = n/10 + n%10
+    For bishop = "a1" and pawn = "c3", the output should be
+    solution(bishop, pawn) = true.
+
+    For bishop = "h1" and pawn = "h3", the output should be
+    solution(bishop, pawn) = false.*/
+    fun solution122(bishop: String, pawn: String) = abs(pawn[0].code - bishop[0].code) == abs(pawn[1].code - bishop[1].code)
 }
