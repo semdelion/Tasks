1c658d1cd78db9217f818ad51aefbb1055c579dc
Semdelion <semdelion@gmail.com>
2024-04-30 21:25:08 +0700

    113

--- Kotlin/CodeSignal/src/theCore/_13_WaterfallOfIntegration.kt
+++ Kotlin/CodeSignal/src/theCore/_13_WaterfallOfIntegration.kt
@@ -109,6 +109,61 @@ object _13_WaterfallOfIntegration {
                                             mutableListOf(  0, 1, 0, 1),
                                             mutableListOf( -1, 1, 0, 0),
                                             mutableListOf( -1, 0, 0, 1)))}")
+
+
+
+        /*println("113)  Is Information Consistent?\n" +
+                "      input: nonogramField = \n" +
+                "             [\"-\",\"-\",\"-\",\"-\",\"-\",\"-\",\"-\",\"-\"]\n" +
+                "             [\"-\",\"-\",\"-\",\"2\",\"2\",\"1\",\"-\",\"1\"]\n" +
+                "             [\"-\",\"-\",\"-\",\"2\",\"1\",\"1\",\"3\",\"3\"]\n" +
+                "             [\"-\",\"3\",\"1\",\"#\",\"#\",\"#\",\".\",\"#\"]\n" +
+                "             [\"-\",\"-\",\"2\",\"#\",\"#\",\".\",\".\",\".\"]\n" +
+                "             [\"-\",\"-\",\"2\",\".\",\".\",\".\",\"#\",\"#\"]\n" +
+                "             [\"-\",\"1\",\"2\",\"#\",\".\",\".\",\"#\",\"#\"]\n" +
+                "             [\"-\",\"-\",\"5\",\"#\",\"#\",\"#\",\"#\",\"#\"]\n" +
+                "             size: 5\n" +
+                "      result: ${solution113(5, mutableListOf(
+                    mutableListOf("-","-","-","-","-","-","-","-"),
+                    mutableListOf("-","-","-","2","2","1","-","1"),
+                    mutableListOf("-","-","-","2","1","1","3","3"),
+                    mutableListOf("-","3","1","#","#","#",".","#"),
+                    mutableListOf("-","-","2","#","#",".",".","."),
+                    mutableListOf("-","-","2",".",".",".","#","#"),
+                    mutableListOf("-","1","2","#",".",".","#","#"),
+                    mutableListOf("-","-","5","#","#","#","#","#")))}")*/
+        println("113)  Is Information Consistent?\n" +
+        "      input: nonogramField = \n" +
+        "             [\"-\",\"-\",\"-\",\"-\",\"-\",\"-\",\"-\",\"-\",\"-\",\"-\",\"-\",\"-\",\"-\",\"-\"]\n" +
+        "             [\"-\",\"-\",\"-\",\"-\",\"-\",\"-\",\"-\",\"-\",\"-\",\"-\",\"-\",\"-\",\"-\",\"-\"]\n" +
+        "             [\"-\",\"-\",\"-\",\"-\",\"-\",\"-\",\"-\",\"-\",\"-\",\"-\",\"-\",\"-\",\"-\",\"-\"]\n" +
+        "             [\"-\",\"-\",\"-\",\"-\",\"-\",\"-\",\"-\",\"4\",\"3\",\"-\",\"-\",\"-\",\"-\",\"-\"]\n" +
+        "             [\"-\",\"-\",\"-\",\"-\",\"-\",\"2\",\"2\",\"2\",\"1\",\"1\",\"3\",\"4\",\"2\",\"2\"]\n" +
+        "             [\"-\",\"-\",\"-\",\"-\",\"1\",\".\",\".\",\".\",\".\",\"#\",\".\",\".\",\".\",\".\"]\n" +
+        "             [\"-\",\"-\",\"-\",\"-\",\"5\",\".\",\".\",\"#\",\"#\",\"#\",\"#\",\"#\",\".\",\".\"]\n" +
+        "             [\"-\",\"-\",\"-\",\"-\",\"7\",\".\",\"#\",\"#\",\"#\",\"#\",\"#\",\"#\",\"#\",\".\"]\n" +
+        "             [\"-\",\"-\",\"-\",\"-\",\"9\",\"#\",\"#\",\"#\",\"#\",\"#\",\"#\",\"#\",\"#\",\"#\"]\n" +
+        "             [\"1\",\"1\",\"1\",\"1\",\"1\",\"#\",\".\",\"#\",\".\",\"#\",\".\",\"#\",\".\",\"#\"]\n" +
+        "             [\"-\",\"-\",\"-\",\"-\",\"1\",\".\",\".\",\".\",\".\",\"#\",\".\",\".\",\".\",\".\"]\n" +
+        "             [\"-\",\"-\",\"-\",\"-\",\"1\",\".\",\".\",\".\",\".\",\"#\",\".\",\".\",\".\",\".\"]\n" +
+        "             [\"-\",\"-\",\"-\",\"1\",\"1\",\".\",\".\",\"#\",\".\",\"#\",\".\",\".\",\".\",\".\"]\n" +
+        "             [\"-\",\"-\",\"-\",\"-\",\"3\",\".\",\".\",\"#\",\"#\",\"#\",\".\",\".\",\".\",\".\"]\n" +
+        "             size: 9\n" +
+        "      result: ${solution113(9, mutableListOf(
+                                                    mutableListOf("-","-","-","-","-","-","-","-","-","-" ,"-","-","-","-"),
+                                                    mutableListOf("-","-","-","-","-","-","-","-","-","-" ,"-","-","-","-"),
+                                                    mutableListOf("-","-","-","-","-","-","-","-","-","-" ,"-","-","-","-"),
+                                                    mutableListOf("-","-","-","-","-","-","-","4","3","-" ,"-","-","-","-"),
+                                                    mutableListOf("-","-","-","-","-","2","2","2","1","9","3","4","2","2"),
+                                                    mutableListOf("-","-","-","-","1",".",".",".",".","#",".",".",".","."),
+                                                    mutableListOf("-","-","-","-","5",".",".","#","#","#","#","#",".","."),
+                                                    mutableListOf("-","-","-","-","7",".","#","#","#","#","#","#","#","."),
+                                                    mutableListOf("-","-","-","-","9","#","#","#","#","#","#","#","#","#"),
+                                                    mutableListOf("1","1","1","1","1","#",".","#",".","#",".","#",".","#"),
+                                                    mutableListOf("-","-","-","-","1",".",".",".",".","#",".",".",".","."),
+                                                    mutableListOf("-","-","-","-","1",".",".",".",".","#",".",".",".","."),
+                                                    mutableListOf("-","-","-","1","1",".",".","#",".","#",".",".",".","."),
+                                                    mutableListOf("-","-","-","-","3",".",".","#","#","#",".",".",".",".")))}")
     }
 
     /**
@@ -472,5 +527,109 @@ object _13_WaterfallOfIntegration {
         }
         return true
     }
+
+    /**
+     * Correct Nonogram
+     * @see "https://app.codesignal.com/arcade/code-arcade/waterfall-of-integration/jkuhbNn7AdeCC7z7R"
+     * @return Is Information Consistent?
+     * * @sample A nonogram is also known as Paint by Numbers and Japanese Crossword. The aim in this puzzle is to color the grid into black and white squares. At the top of each column, and at the side of each row, there are sets of one or more numbers which describe the runs of black squares in that row/column in exact order. For example, if you see 10 1 along some column/row, this indicates that there will be a run of exactly ten black squares, followed by one or more white squares, followed by a single black square. The cells along the edges of the grid can also be white.
+
+    You are given a square nonogram of size size. Its grid is given as a square matrix nonogramField of size (size + 1) / 2 + size, where the first (size + 1) / 2 cells of each row and and each column define the numbers for the corresponding row/column, and the rest size × size cells define the the grid itself.
+
+    Determine if the given nonogram has been solved correctly.
+
+    Note: here / means integer division.
+
+    Example
+
+    For size = 5 and
+
+    nonogramField =
+    ["-", "-", "-", "-", "-", "-", "-", "-"],
+    ["-", "-", "-", "2", "2", "1", "-", "1"],
+    ["-", "-", "-", "2", "1", "1", "3", "3"],
+    ["-", "3", "1", "#", "#", "#", ".", "#"],
+    ["-", "-", "2", "#", "#", ".", ".", "."],
+    ["-", "-", "2", ".", ".", ".", "#", "#"],
+    ["-", "1", "2", "#", ".", ".", "#", "#"],
+    ["-", "-", "5", "#", "#", "#", "#", "#"]
+    the output should be solution(size, nonogramField) = true;
+
+    For size = 5 and
+
+    nonogramField =
+    ["-", "-", "-", "-", "-", "-", "-", "-"],
+    ["-", "-", "-", "-", "-", "1", "-", "-"],
+    ["-", "-", "-", "3", "3", "2", "5", "5"],
+    ["-", "-", "3", ".", ".", ".", "#", "#"],
+    ["-", "2", "2", "#", "#", "#", "#", "#"],
+    ["-", "-", "5", "#", "#", "#", "#", "#"],
+    ["-", "-", "5", "#", "#", "#", "#", "#"],
+    ["-", "-", "2", ".", ".", ".", "#", "#"]
+    the output should be solution(size, nonogramField) = false.
+
+    There are three mistakes in the nonogram:
+
+    In the 5th (1-based) row there are numbers ["-", "2", "2"], so there should be two runs of 2 black squares separated by at least one white square. However, there is only one run of 5 black squares.
+    In the 6th column there are numbers ["-", "1", "2"], so there should be a run of exactly 1 black square, followed by one or more white squares, followed by another 2 black squares. However, there is a single run of 3 black squares not separated by white ones.
+    Finally, in the 4th row there are numbers ["-", "-", "3"], so there should be a single run of exactly 3 black squares. However, there is just a 2-square run in that row.*/
+    fun solution113(size: Int, nonogramField: MutableList<MutableList<String>>): Boolean {
+        val start = (size + 1) / 2
+        val end = (size + 1) / 2 + size
+        for (i in start until end) {
+            var j = start
+            for (k in 0 until start) {
+                if(nonogramField[i][k][0].isDigit()) {
+                    var count = nonogramField[i][k].toInt()
+                    while (j < end && nonogramField[i][j] == ".") j++
+
+                    if (nonogramField[i][j - 1] == "#")
+                        return false
+
+                    while (j < end && count > 0) {
+                        if (nonogramField[i][j] != "#")
+                            return false
+                        count--
+                        j++
+                    }
+                    if (count > 0)
+                        return false
+                }
+            }
+            while (j < end) {
+                if (nonogramField[i][j] == "#")
+                    return false
+                j++
+            }
+        }
+        for (j in start until end) {
+            var i = start
+            for (k in 0 until start) {
+                if(nonogramField[k][j][0].isDigit()) {
+                    var count = nonogramField[k][j].toInt()
+                    while (i < end && nonogramField[i][j] == ".") i++
+
+                    if (nonogramField[i - 1][j] == "#")
+                        return false
+
+                    while (i < end && count > 0) {
+                        if (nonogramField[i][j] != "#")
+                            return false
+                        count--
+                        i++
+                    }
+                    if (count > 0)
+                        return false
+                }
+            }
+            while (i < end) {
+                if (nonogramField[i][j] == "#")
+                    return false
+                i++
+            }
+        }
+        return true
+    }
+
 
 }
