3bc79dec4a0a145475109c75088726656391a971
Semdelion <semdelion@gmail.com>
2024-05-04 14:23:45 +0700

    130

--- Kotlin/CodeSignal/src/theCore/_16_TimeRiver.kt
+++ Kotlin/CodeSignal/src/theCore/_16_TimeRiver.kt
@@ -2,20 +2,28 @@ package theCore
 
 object _16_TimeRiver {
     fun run() {
-        /*println("130) Add Two Digits\n" +
-                "      input: \n" +
-                "      result: ${_16_TimeRiver.solution130(29)}\n")*/
+        println("130) Valid Time\n" +
+                "      input: time = \"25:51\" \n" +
+                "      result: ${solution130("25:51")}\n")
     }
 
     /**
-     * Add Two Digits
-     * @see "https://app.codesignal.com/arcade/code-arcade/intro-gates/wAGdN6FMPkx7WBq66"
-     * @return Add Two Digits
-     * * @sample You are given a two-digit integer n. Return the sum of its digits.
+     * Valid Time
+     * @see "https://app.codesignal.com/arcade/code-arcade/time-river/ywMyCTspqGXPWRZx5"
+     * @return Valid Time
+     * * @sample Check if the given string is a correct time representation of the 24-hour clock.
 
     Example
 
-    For n = 29, the output should be
-    solution(n) = 11.*/
-    fun solution130(n: Int): Int = n/10 + n%10
+    For time = "13:58", the output should be
+    solution(time) = true;
+    For time = "25:51", the output should be
+    solution(time) = false;
+    For time = "02:76", the output should be
+    solution(time) = false.
+     */
+    fun solution130(time: String): Boolean {
+        val regex = Regex("^((([0-1]\\d)|2[0-3]):([0-5]\\d))$")
+        return regex.findAll(time).count() == 1
+    }
 }
