734c63ee5b973eeeb5d99cfd728991645a186ef4
Semdelion <semdelion@gmail.com>
2024-05-07 10:02:17 +0700

    118

--- Kotlin/CodeSignal/src/theCore/_14_SortingOutpost.kt
+++ Kotlin/CodeSignal/src/theCore/_14_SortingOutpost.kt
@@ -1,7 +1,5 @@
 package theCore
 
-import kotlin.math.min
-
 object _14_SortingOutpost {
     fun run() {
         println("114) Shuffled Array\n" +
@@ -16,6 +14,14 @@ object _14_SortingOutpost {
         println("117) Boxes Packing\n" +
                 "      input: length = [1, 3, 2], width = [1, 3, 2], and height = [1, 3, 2]\n" +
                 "      result: ${_14_SortingOutpost.solution117(mutableListOf(1, 3, 2), mutableListOf(1, 3, 2), mutableListOf(1, 3, 2))}\n")
+        println("118) Maximum Sum\n" +
+                "      input: length = a[9, 7, 2, 4, 4], q=[[1, 3], [1, 4], [0, 2]]\n" +
+                "      result: ${_14_SortingOutpost.solution118(mutableListOf(9, 7, 2, 4, 4), 
+                    mutableListOf(
+                        mutableListOf(1, 3),
+                        mutableListOf(1, 4),
+                        mutableListOf(0, 2)
+                ))}\n")
     }
 
     /**
@@ -126,6 +132,39 @@ object _14_SortingOutpost {
                 return false
         }
         return true
+    }
+
+    /**
+     * Maximum Sum
+     * @see "https://app.codesignal.com/arcade/code-arcade/sorting-outpost/64koZSDqndwYxFZj6"
+     * @return Maximum Sum
+     * * @sample You are given an array of integers a. A range sum query is defined by a pair of non-negative integers l and r (l <= r). The output to a range sum query on the given array a is the sum of all the elements of a that have indices from l to r, inclusive.
+
+    You have the array a and a list of range sum queries q. Find an algorithm that can rearrange the array a in such a way that the total sum of all of the query outputs is maximized, and return this total sum.
+
+    Example
+
+    For a = [9, 7, 2, 4, 4] and q = [[1, 3], [1, 4], [0, 2]], the output should be
+    solution(a, q) = 62.
+
+    You can get this sum if the array a is rearranged to be [2, 9, 7, 4, 4]. In that case, the first range sum query [1, 3] returns the sum 9 + 7 + 4 = 20, the second query [1, 4] returns the sum 9 + 7 + 4 + 4 = 24, and the third query [0, 2] returns the sum 2 + 9 + 7 = 18. The total sum will be 20 + 24 + 18 = 62.
+     */
+    fun solution118(a: MutableList<Int>, q: MutableList<MutableList<Int>>): Int {
+        val frequencies = MutableList(a.size){0}
+        for(i in 0 until q.size) {
+            for(j in q[i][0]..q[i][1]) {
+                frequencies[j]++
+            }
+        }
+        frequencies.sort()
+        a.sort()
+        var result = 0
+        var i = frequencies.size - 1
+        while(i >= 0 && frequencies[i] != 0) {
+            result += frequencies[i]*a[i]
+            i--
+        }
+        return result
     }
 
 }
